c4752dc44863493a43b517edf2f3f34def584d85
saintxseiya <francesco.ruiu@e-saint.org>
2015-03-24 18:45:12 +0100

    first commit

---
+++ Mscservice/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := Mscservice
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := mscservice.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
